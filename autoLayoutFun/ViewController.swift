a8be0199f0a6708629ee87d35ae239defd84631c
Chucka, Zachary Tyler <zchucka@ath-allena-lap.gonzaga.edu>
2018-10-04 10:03:37 -0700

    Initial Commit

---
+++ autoLayoutFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayoutFun
+//
+//  Created by Chucka, Zachary Tyler on 10/4/18.
+//  Copyright © 2018 Chucka, Zachary Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
